8a6fcd333073d086c63bc616ee9a08829d3001a8
Hermann Ólafsson <hermann.olafs@gmail.com>
2022-04-28 09:20:33 +0200

    writing message to a file

--- slack/slack.go
+++ slack/slack.go
@@ -4,16 +4,19 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
 	"encore.dev/rlog"
-	"github.com/slack-go/slack/slackevents"
 
+	"github.com/slack-go/slack/slackevents"
 	"github.com/slack-go/slack"
 )
 
-const cowart = "Moo! %s"
+var (
+	api *slack.Client
+)
 
-var api *slack.Client
 
 var secrets struct {
 	AppToken      string // xapp
@@ -23,72 +26,110 @@ var secrets struct {
 
 func init() {
 	rlog.Debug("Init method invoked")
-	api = slack.New(secrets.AppToken,
-		slack.OptionAppLevelToken(secrets.BotToken),
-		slack.OptionDebug(true),
+	authenticateWithSlack()
+}
+
+func authenticateWithSlack() {
+	rlog.Info("Initializing slack authentication")
+	api = slack.New(secrets.BotToken,
+		slack.OptionAppLevelToken(secrets.AppToken),
 	)
 
-	test, err := api.AuthTest()
-	rlog.Debug("Got this output from AuthTest: ", "test", test)
-	rlog.Debug("Got this err from AuthTest: ", "err", err)
-	if err != nil {
+	if _, err := api.AuthTest(); err != nil {
+		rlog.Error("Got this err from AuthTest: ", "err", err)
 		panic("issues: " + err.Error())
 	}
-
+	rlog.Info("Authenticated with Slack!")
 }
 
 //encore:api public raw path=/msgr
-func Msgr(w http.ResponseWriter, r *http.Request)  {
+func Msgr(w http.ResponseWriter, r *http.Request) {
+	body, eventsAPIEvent, idontKnowwhatThisVariableIs := parseEventFromBody(w, r)
+	if idontKnowwhatThisVariableIs {
+		return
+	}
+
+	parseEventsApiEvent(w, eventsAPIEvent, body)
+}
+
+func parseEventsApiEvent(w http.ResponseWriter, eventsAPIEvent slackevents.EventsAPIEvent, body []byte) {
+	switch eventsAPIEvent.Type {
+	case slackevents.CallbackEvent: 	consumeSlackCallBackEvent(w, body, eventsAPIEvent)
+	case slackevents.URLVerification: 	slackURLVerification(w, body)
+	default:
+		rlog.Info("No case for event:", "eventsAPIEvent", eventsAPIEvent)
+	}
+}
+
+func parseEventFromBody(w http.ResponseWriter, r *http.Request) ([]byte, slackevents.EventsAPIEvent, bool) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		rlog.Error("failed opening body")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
 	if verifySlackSigning(w, r, body) {
-		return
+		http.Error(w, "failed verifying slack signing token", http.StatusUnauthorized)
+		return nil, slackevents.EventsAPIEvent{}, true
 	}
 
 	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
 	if err != nil {
+		rlog.Error("Error parsing event :(")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, slackevents.EventsAPIEvent{}, true
 	}
+	return body, eventsAPIEvent, false
+}
 
-	switch eventsAPIEvent.Type {
-	case slackevents.CallbackEvent:
-		consumeSlackCallBackEvent(w, eventsAPIEvent)
-	case slackevents.URLVerification:
-		slackURLVerification(w, body)
-	default:
-		rlog.Debug("No case for event:", "eventsAPIEvent", eventsAPIEvent)
+func consumeSlackCallBackEvent(w http.ResponseWriter, body []byte, event slackevents.EventsAPIEvent) {
+	rlog.Debug("Event!", "event", event)
+	innerEvent := event.InnerEvent
+
+	switch ev := innerEvent.Data.(type) {
+	case *slackevents.AppMentionEvent: 	consumeAppMention(w, *ev)
+	case *slackevents.MessageEvent: 	consumeMessageEvent(w, *ev, body)
 	}
 }
 
-func consumeSlackCallBackEvent(w http.ResponseWriter, event slackevents.EventsAPIEvent) {
-	innerEvent := event.InnerEvent
-	switch ev := innerEvent.Data.(type) {
-	case *slackevents.AppMentionEvent:
-		_, _, err := api.PostMessage(ev.Channel, slack.MsgOptionText("Yes", false))
-		if err != nil {
-			rlog.Debug(err.Error())
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
+func consumeAppMention(w http.ResponseWriter, event slackevents.AppMentionEvent) {
+	err := api.AddReaction("cake", slack.NewRefToMessage(event.Channel, event.TimeStamp))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+
+}
+
+func consumeMessageEvent(w http.ResponseWriter, event slackevents.MessageEvent, body []byte) {
+	err := api.AddReaction("mrclean", slack.NewRefToMessage(event.Channel, event.TimeStamp))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+
+	fileName := strings.Replace(event.Text," ", "_", -1)
+
+	f, err := os.Create(fileName + ".json")
+	if err != nil {
+		rlogAndHttpError("Error creating the file", w, err, http.StatusInternalServerError)
+	}
+	if _, err := f.Write(body); err != nil {
+		rlogAndHttpError("Error writing to the file", w, err, http.StatusInternalServerError)
+	}
+
 }
 
 func slackURLVerification(w http.ResponseWriter, body []byte) {
-	rlog.Debug("Got a url verify request", )
 	var r *slackevents.ChallengeResponse
-	err := json.Unmarshal([]byte(body), &r)
+	err := json.Unmarshal(body, &r)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		rlogAndHttpError("", w, err, http.StatusInternalServerError)
 	}
 	w.Header().Set("Content-Type", "text")
 	_, err = w.Write([]byte(r.Challenge))
 	if err != nil {
-		rlog.Error("Error writing challenge response: ", "err", err)
-		http.Error(w, "Error writing challenge response: " +err.Error(), http.StatusInternalServerError)
-		return 
+		rlogAndHttpError("Error writing challenge response: ", w, err, http.StatusInternalServerError)
 	}
 }
 
@@ -111,4 +152,9 @@ func verifySlackSigning(w http.ResponseWriter, r *http.Request, body []byte) boo
 		return true
 	}
 	return false
+}
+
+func rlogAndHttpError(message string, w http.ResponseWriter, err error, statusCode int) {
+	rlog.Error(message, "err", err)
+	http.Error(w, message + err.Error(), statusCode)
 }
